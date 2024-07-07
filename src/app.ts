95c73496c116ae0b38ddf76a8f197ca8a68938c2
Naymur Rahman <nrtusher1@gmail.com>
2024-07-07 23:07:13 +0600

    Changes on app ts and userController

--- src/app.ts
+++ src/app.ts
@@ -5,13 +5,19 @@ import { UserRoutes } from './app/modules/users/user.route';
 const app: Application = express();
 
 //parsers
+
 app.use(express.json());
 app.use(cors());
 
-app.use(UserRoutes);
+app.use('/api', UserRoutes);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to TGAHS! 🤡🤡');
 });
 
+app.use('*', (req, res) => {
+  res.status(404).json({
+    message: 'API Not found',
+  });
+});
 export default app;
